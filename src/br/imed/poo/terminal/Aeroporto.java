c8f9057e2225e6723e5417fd80cd2057eb0242a4
Júlia <juliaspaderbianchi@gmail.com>
2021-11-01 21:34:42 -0300

    criando novas classes

---
+++ src/br/imed/poo/terminal/Aeroporto.java
@@ -0,0 +1,28 @@
+package br.imed.poo.terminal;
+
+public class Aeroporto {
+	private int codigo;
+	private int tamanhoPista;
+	private String cidade;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public int getTamanhoPista() {
+		return tamanhoPista;
+	}
+	public void setTamanhoPista(int tamanhoPista) {
+		this.tamanhoPista = tamanhoPista;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	
+}
---
+++ src/br/imed/poo/terminal/TerminalLogistico.java
@@ -0,0 +1,22 @@
+package br.imed.poo.terminal;
+
+
+public class TerminalLogistico {
+	private String cidade;
+	private String veiculos;
+	
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getVeiculos() {
+		return veiculos;
+	}
+	public void setVeiculos(String veiculos) {
+		this.veiculos = veiculos;
+	}
+	
+	
+}
